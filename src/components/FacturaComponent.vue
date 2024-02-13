d70939d65fcb544df0e91122a5f87ad69c716f1f
junior campos <junior12campos18@gmail.com>
2024-02-13 10:45:59 -0500

    cambios a la tabla y a la pagina incluyendo campos nuevos

--- src/components/FacturaComponent.vue
+++ src/components/FacturaComponent.vue
@@ -8,12 +8,13 @@
       <input type="number" id="cantidad" v-model.number="cantidadProducto" required>
 
       <label for="precio">Precio Unitario:</label>
-      <input type="number" id="precio" v-model.number="precioProducto" required>
+      <input type="text" id="precio" v-model="precioProducto" required>
 
       <button type="submit" id="btn_click">Agregar Producto</button>
     </form>
 
     <h2>Detalle de la Factura</h2>
+
     <table>
       <thead>
         <tr>
@@ -27,14 +28,14 @@
         <tr v-for="(producto, index) in productos" :key="index">
           <td>{{ producto.nombre }}</td>
           <td>{{ producto.cantidad }}</td>
-          <td>{{ producto.precio }}</td>
-          <td>{{ producto.total }}</td>
+          <td>{{ producto.precio }} pesos</td>
+          <td>{{ producto.total }} pesos</td>
         </tr>
       </tbody>
       <tfoot>
         <tr>
           <td colspan="3" style="text-align: right;"><strong>Total Factura:</strong></td>
-          <td>{{ totalFactura }}</td>
+          <td>{{ totalFactura }} pesos</td>
         </tr>
         <tr>
           <td colspan="4">
@@ -44,6 +45,56 @@
         </tr>
       </tfoot>
     </table>
+
+    <div class="total-container">
+      <div class="total">
+        <strong>Total Factura:</strong> {{ totalFactura }} pesos
+      </div>
+      <div class="precio-pagar">
+        <strong>Precio a pagar:</strong> {{ totalFactura }} pesos
+      </div>
+    </div>
+
+    <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+      <!-- Datos del remitente -->
+      <div>
+        <h3>Remitente</h3>
+        <label for="nombreRemitente">Nombre:</label>
+        <input type="text" id="nombreRemitente" v-model="nombreRemitente">
+
+        <label for="idRemitente">Identificación:</label>
+        <input type="text" id="idRemitente" v-model="idRemitente">
+
+        <label for="telefonoRemitente">Teléfono:</label>
+        <input type="text" id="telefonoRemitente" v-model="telefonoRemitente">
+      </div>
+
+      <!-- Datos del destinatario -->
+      <div>
+        <h3>Destinatario</h3>
+        <label for="nombreDestinatario">Nombre:</label>
+        <input type="text" id="nombreDestinatario" v-model="nombreDestinatario">
+
+        <label for="idDestinatario">Identificación:</label>
+        <input type="text" id="idDestinatario" v-model="idDestinatario">
+
+        <label for="telefonoDestinatario">Teléfono:</label>
+        <input type="text" id="telefonoDestinatario" v-model="telefonoDestinatario">
+      </div>
+
+      <!-- Datos de la factura -->
+      <div>
+        <h3>Datos de la Factura</h3>
+        <label for="numeroFactura">Número de Factura:</label>
+        <input type="text" id="numeroFactura" v-model="numeroFactura" readonly>
+
+        <label for="fechaFactura">Fecha de Factura:</label>
+        <input type="date" id="fechaFactura" v-model="fechaFactura" readonly>
+
+        <label for="vencimientoFactura">Vencimiento de Factura:</label>
+        <input type="date" id="vencimientoFactura" v-model="vencimientoFactura" readonly>
+      </div>
+    </div>
 
     <iframe ref="pdfPreview" style="display: none; width: 40%; height: 400px;"></iframe>
     <button @click="previewPDF">Vista previa de la factura</button>
@@ -60,10 +111,18 @@ export default {
     return {
       nombreProducto: '',
       cantidadProducto: 0,
-      precioProducto: 0,
+      precioProducto: '',
       productos: [],
       observaciones: '',
-      numeroFactura: 1
+      numeroFactura: 1,
+      fechaFactura: '',
+      vencimientoFactura: '',
+      nombreRemitente: '',
+      idRemitente: '',
+      telefonoRemitente: '',
+      nombreDestinatario: '',
+      idDestinatario: '',
+      telefonoDestinatario: ''
     };
   },
   computed: {
@@ -73,17 +132,25 @@ export default {
   },
   methods: {
     agregarProducto() {
-      const total = this.cantidadProducto * this.precioProducto;
+
+      const precioNumerico = parseFloat(this.precioProducto.replace(/,/g, ''));
+
+      // Verificar si el precio es un número válido
+      if (isNaN(precioNumerico)) {
+        alert('Por favor, ingrese un precio válido.');
+        return;
+      }
+      const total = this.cantidadProducto * precioNumerico;
       this.productos.push({
         nombre: this.nombreProducto,
         cantidad: this.cantidadProducto,
-        precio: this.precioProducto,
+        precio: precioNumerico,
         total: total
       });
       this.numeroFactura++;
       this.nombreProducto = '';
       this.cantidadProducto = 0;
-      this.precioProducto = 0;
+      this.precioProducto = '';
     },
     previewPDF() {
       const doc = this.generatePDF();
@@ -91,7 +158,6 @@ export default {
       this.$refs.pdfPreview.src = pdfDataUri;
       this.$refs.pdfPreview.style.display = 'block';
     },
-    
     downloadPDF() {
       const doc = this.generatePDF();
       const filename = 'FACTURA.pdf';
@@ -99,47 +165,58 @@ export default {
       window.open(doc.output('bloburl'));
     },
     generatePDF() {
-      const pageSize = { width: 216 , height: 279  };
+      const pageSize = { width: 216, height: 279 };
       const doc = new jsPDF({ format: [pageSize.width, pageSize.height] });
 
-      // Agregar imagen, texto y línea de encabezado
+      // Encabezado - Título y datos de la empresa
       const img = new Image();
       img.src = require('@/assets/jj.jpg'); 
-      doc.addImage(img, 'PNG', 8, 5, 25, 25);
-      doc.text('Yuster', 30, 17);
-      doc.text('Nit: 147852369', 30, 22);
-      doc.line(10, 30, 140, 30);
-
-      // Agregar título y dirección
+      doc.addImage(img, 'PNG', 180, 5, 25, 25);
       doc.setFontSize(10);
-      doc.text('YUSCORP', 10, 35);
-      doc.text('Dirección: Calle 123, Ciudad', 10, 39);
-      doc.text('Teléfono: 123-456-789', 10, 43);
+      doc.setFont('helvetica', 'bold');
+      doc.text('YUSCORP', 164, 17);
+      doc.text('Nit: 147852369', 158, 20);
+      doc.setFontSize(20);
+      doc.line(10, 28, 205, 28);
+      doc.setFont('helvetica', 'bold');
+      doc.text('Factura', 12, 20);
 
-      //fecha y hora
-      const numeroFactura = `N° Factura: ${this.numeroFactura}`;
-      const now = new Date();
-      const fechaHoraActual = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
-      const maxWidth = doc.internal.pageSize.width;
-      const textWidth = doc.getStringUnitWidth(numeroFactura) * doc.internal.getFontSize();
-      const xPosition = maxWidth - textWidth - -10; 
+      // Datos del remitente
+      doc.setFontSize(12);
+      doc.setFont('bold');
+      doc.text('Remitente', 10, 35);
+      doc.text(`Nombre: ${this.nombreRemitente}`, 10, 40);
+      doc.text(`Identificación: ${this.idRemitente}`, 10, 45);
+      doc.text(`Teléfono: ${this.telefonoRemitente}`, 10, 50);
 
-      // fecha y hora posicion
-      doc.text(numeroFactura, xPosition, 38);
-      doc.text(fechaHoraActual, xPosition, 43);
+      // Datos del destinatario
+      doc.text('Destinatario', 80, 35);
+      doc.text(`Nombre: ${this.nombreDestinatario}`, 80, 40);
+      doc.text(`Identificación: ${this.idDestinatario}`, 80, 45);
+      doc.text(`Teléfono: ${this.telefonoDestinatario}`, 80, 50);
 
-      //tabla de datos de factura
+      // Datos de la factura
+      doc.text('Datos de la Factura', 150, 35);
+      doc.text(`N° Factura: ${this.numeroFactura}`, 150, 40);
+      doc.text(`Fecha: ${this.fechaFactura}`, 150, 45);
+      doc.text(`Vencimiento: ${this.vencimientoFactura}`, 150, 50);
+
+      // Línea de separación
+      doc.setLineWidth(0.5);
+      doc.line(10, 55, pageSize.width - 10, 55);
+
+      // Detalle de la factura - Tabla de productos
       const data = [
         ['Nombre', 'Cantidad', 'Precio', 'Total'],
         ...this.productos.map(producto => [
           producto.nombre,
           producto.cantidad,
-          producto.precio.toLocaleString('es-ES', { style: 'currency', currency: 'COP' }),
-          producto.total.toLocaleString('es-ES', { style: 'currency', currency: 'COP' }),
+          `${producto.precio.toLocaleString('es-ES')} pesos`,
+          `${producto.total.toLocaleString('es-ES')} pesos`,
         ])
       ];
 
-      const tableStartY = 50;
+      const tableStartY = 60;
 
       doc.autoTable({
         startY: tableStartY,
@@ -148,24 +225,7 @@ export default {
         theme: 'grid'
       });
 
-      // Agregar líneas en todas las páginas
-      const agregarLineasEnTodasLasPaginas = () => {
-        doc.setLineWidth(1);
-        doc.line(0, 0, pageSize.width, 0); // Línea superior
-        doc.line(0, pageSize.height, pageSize.width, pageSize.height); // Línea inferior
-        doc.line(0, 0, 0, pageSize.height); // Línea izquierda
-        doc.line(pageSize.width, 0, pageSize.width, pageSize.height); // Línea derecha
-      };
-
-      if (doc.autoTable.previous.finalY > pageSize.height - 10) {
-        doc.addPage();
-        agregarLineasEnTodasLasPaginas();
-      }
-
-      if (doc.autoTable.previous.finalY > pageSize.height - 10) {
-        doc.addPage();
-        agregarLineasEnTodasLasPaginas();
-      }
+      // Observaciones
       const observacionesXPosition = 10;
       const observacionesYPosition = doc.autoTable.previous.finalY + 10;
       const observacionesWidth = pageSize.width - 20;
@@ -180,11 +240,19 @@ export default {
       doc.text(this.observaciones, observacionesXPosition + 5, observacionesYPosition + 10, { maxWidth: observacionesWidth - 10 });
       const totalXPosition = 165;
       doc.text('Total:', totalXPosition, observacionesYPosition + observacionesHeight + 5);
-      doc.text(this.totalFactura.toLocaleString('es-ES', { style: 'currency', currency: 'COP' }), totalXPosition, observacionesYPosition + observacionesHeight + 10);
+      doc.text(`${this.totalFactura.toLocaleString('es-ES')} pesos`, totalXPosition, observacionesYPosition + observacionesHeight + 10);
       doc.text('Gracias por su compra', 110, observacionesYPosition + observacionesHeight + 20, { align: 'center', fontSize: 12 });
 
       return doc;
     }
+  },
+  mounted() {
+    const currentDate = new Date();
+    this.fechaFactura = currentDate.toISOString().substr(0, 10);
+
+    const dueDate = new Date(currentDate);
+    dueDate.setDate(dueDate.getDate() + 30);
+    this.vencimientoFactura = dueDate.toISOString().substr(0, 10);
   }
 }
 </script>
@@ -192,5 +260,21 @@ export default {
 <style scoped>
 .observaciones {
   background-color: aqua;
+}
+
+.total-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.total {
+  background-color: #f2f2f2;
+  padding: 10px;
+}
+
+.precio-pagar {
+  background-color: #f2f2f2;
+  padding: 10px;
 }
 </style>
